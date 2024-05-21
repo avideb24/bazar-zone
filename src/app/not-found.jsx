ad28b31817846c232e8933e38ca1b6c381cefd68
avideb24 <debavi24@gmail.com>
2024-05-21 16:09:08 +0600

    updated not-found page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const NotFoundPage = () => {
     return (
         <div>
-            <h1 className="text-2xl font-semibold">Not Found</h1>
+            <h1 className="text-2xl font-bold">Not Found</h1>
             <Link href={'/'}>
                 <button className="px-3 py-1 font-semibold bg-primary">Go Home</button>
             </Link>
