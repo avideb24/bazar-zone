9839492b17a4ec1ce109d6fb5601b0ae405af683
avideb24 <debavi24@gmail.com>
2024-06-12 14:44:30 +0600

    updated

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -3,6 +3,7 @@ import getSingleProduct from '@/libs/getSingleProduct';
 import ProductImages from '@/components/pages/ProductDetails/ProductImages/ProductImages';
 import { RiArrowRightSLine } from "react-icons/ri";
 import ProductRating from '@/components/pages/ProductDetails/ProductRating/ProductRating';
+import Button2 from '@/components/shared/Button2/Button2';
 
 const ProductPage = async ({ params }) => {
 
@@ -15,6 +16,14 @@ const ProductPage = async ({ params }) => {
     return (
         <section>
 
+            <div className='flex items-center gap-2 pt-4 pb-10'>
+                <span className='opacity-75'>All</span>
+                <span><RiArrowRightSLine /></span>
+                <span className='opacity-75'>Products</span>
+                <span><RiArrowRightSLine /></span>
+                <span>{product?.title}</span>
+            </div>
+
             <div className='flex flex-col md:flex-row gap-6 md:gap-12 pb-12'>
 
                 {/* image gallery */}
@@ -22,11 +31,6 @@ const ProductPage = async ({ params }) => {
 
                 {/* product details */}
                 <div className='space-y-3 font-semibold'>
-                    <div className='flex items-center gap-2 text-base'>
-                        <span>All</span>
-                        <span className='opacity-80 text-xl'><RiArrowRightSLine /></span>
-                        <span>{product?.title}</span>
-                    </div>
                     {
                         product?.stock > 20 ?
                         <p className='bg-green-600 text-white text-xs px-3 py-1 inline-block'>In Stock</p>
@@ -35,16 +39,21 @@ const ProductPage = async ({ params }) => {
                     }
                     <h2 className='text-2xl md:text-3xl font-semibold'>{product?.title}</h2>
                     <ProductRating rating={'4.3'} />
-                    <p>Price: ${product?.price}</p>
+                    <p className='flex items-center gap-2'>Price: <span className='text-xl md:text-2xl'>${product?.price}</span></p>
                     <div className='flex items-center gap-2 flex-wrap'>
                         <span>Sizes:</span>
                         {
                             product?.sizes.map(size =>
-                                <p key={size} className={`${size?.length < 1 ? "hidden" : ""} px-3 py-1 bg-secondary text-white`}>{size}</p>
+                                <p key={size} className={`${size?.length < 1 ? "hidden" : ""} text-secondary`}>{size}</p>
                             )
                         }
                     </div>
-                    <p>Category: <span className='bg-secondary text-white px-2 py-1'>{product?.category}</span></p>
+                    <div>
+                        <div>
+
+                        </div>
+                        <button><Button2 btnText={'Add To Cart'} /></button>
+                    </div>
                 </div>
 
             </div>
